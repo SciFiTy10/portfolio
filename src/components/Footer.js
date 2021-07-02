b00ef8220288405908dafaeb6380cc55e0200734
Tyler <tythedeveloper10@gmail.com>
2021-07-01 21:25:24 -0500

    add footer section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,25 +2,33 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import { Link } from "react-scroll";
 import * as footerStyles from "../styles/Component/footer.module.scss";
+import { FaAngleUp, FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <footer className={`${footerStyles.footer} ${"navbar-static-bottom"}`}>
       <Container>
         <span className={footerStyles.backToTop}>
-          <Link to="hero" smooth duration={1000}>
-            <i className="fa fa-angle-up fa-2x" aria-hidden="true" />
+          <Link to="welcome" smooth duration={1000}>
+            <FaAngleUp />
           </Link>
         </span>
         <div className={footerStyles.socialLinks}>
           <a
-            key="1"
-            href="https://github.com/cobidev/gatsby-simplefolio"
+            href="https://github.com/SciFiTy10"
             rel="noopener noreferrer"
             target="_blank"
             aria-label="github"
           >
-            <i className="fa fa-refresh fa-inverse" />
+            <FaGithub />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/tyler-ridings-24804585/"
+            rel="noopener noreferrer"
+            target="_blank"
+            aria-label="linkedin"
+          >
+            <FaLinkedin />
           </a>
         </div>
       </Container>
