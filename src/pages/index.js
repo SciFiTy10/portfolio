9e193f53d30124c3b6ccbfe575e4fe5126195b13
Tyler <tythedeveloper10@gmail.com>
2021-07-02 21:45:10 -0500

    add helmet

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,18 @@ import Projects from "../components/Projects";
 import Books from "../components/Books";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
+import { Helmet } from "react-helmet";
 
 // markup
 const IndexPage = () => {
   return (
     <main>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Ty's Portfolio</title>
+        <html lang="en" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Helmet>
       <Welcome />
       <About />
       <Projects />
